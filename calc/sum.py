25f4996bf47b6c4cef5b17137d9a8b1e2f58a978
tpgh02 <tpgh02@kookmin.ac.kr>
2020-06-21 22:39:23 +0900

    Issue#7 If a or b isn't inputted integer,which is float number or string,x and y print 'Please Enter the Integer'

--- calc/sum.py
+++ calc/sum.py
@@ -11,7 +11,8 @@ def application(environ, start_response):
     x = ''
     y = ''
 
-    if a == '' or b == '':
+    try:
+      if a == '' or b == '':
         response_body = html % {
             'x' : 'Please Enter the Value',
             'y' : 'Please Enter the Value',
@@ -19,7 +20,7 @@ def application(environ, start_response):
             'b' : b
         }
 
-    else:
+      else:
         if '' not in [a, b]:
             a, b = int(a), int(b)
             global x 
@@ -32,6 +33,13 @@ def application(environ, start_response):
              'y' : y,
 	     'a' : a,
 	     'b' : b 
+        }
+    except ValueError:
+       response_body = html % {
+             'x' : 'Please Enter the Integer',
+             'y' : 'Please Enter the Integer',
+             'a' : a,
+             'b' : b
         }
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
