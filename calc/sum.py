2e9650f2bba37e6352f80d70024ba772f2274e5a
tpgh02 <tpgh02@kookmin.ac.kr>
2020-06-21 17:44:30 +0900

    Issue#4 locate WSGI:'sum and product' source code in calc

---
+++ calc/sum.py
@@ -0,0 +1,32 @@
+from cgi import parse_qs
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    x = ''
+    y = ''
+
+
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+        global x 
+	global y
+        x = a + b
+        y = a * b
+
+    response_body = html % {
+         'x' : x,
+         'y' : y,
+	 'a' : a,
+	 'b' : b 
+    }
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
